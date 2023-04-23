0abff0a03a1ae8a718df8e2eeb4f8bd3d684bd97
Davi Castro <davialmeida47jd@gmail.com>
2023-04-23 02:36:03 -0300

    feat(clients-app): created database model for clients and created migrate

--- pharma_django/apps/clients/models.py
+++ pharma_django/apps/clients/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Clients(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField(max_length=100)
+    cpf_cnpj = models.CharField(max_length=20)
+    phone = models.CharField(max_length=15)
+    address = models.CharField(max_length=100)
+    city = models.CharField(max_length=100)
+    state = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
